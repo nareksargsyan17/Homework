9eb370f91d37225163f59e05474b925f988774a1
nareksargsyan17 <narek.sargsyan.2000.g@gmai.com>
2022-06-23 13:20:41 +0400

    lesson 13 homework

--- index.js
+++ index.js
@@ -160,3 +160,25 @@ const mostGoalScorer = (team) => {
 };
 
 mostGoalScorer("Real Madrid");
+
+
+function date() {
+	let data = new Date();
+	let day = data.getDay();
+	return day;
+}
+function mig() {
+	let day = date();
+	switch (day) {
+		case 0: day = "Sunday"; break;
+		case 1: day = "Monday"; break;
+		case 2: day = "Tuesday"; break;
+		case 3: day = "Wednesday"; break;
+		case 4: day = "Thursday"; break;
+		case 5: day = "Friday"; break;
+		case 6: day = "Saturday"; break;
+		default: return "error";
+	}
+	return day;
+}
+console.log(mig());
