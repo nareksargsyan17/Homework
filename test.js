5f58fa0ecb5f22aab3344437c35bf81fd9012e64
nareksargsyan17 <narek.sargsyan.2000.g@gmai.com>
2022-07-09 13:06:59 +0400

    My Homework

--- test.js
+++ test.js
@@ -1,113 +1,43 @@
-const school = {
-    class1: 27,
-    class2: 25,
-    class3: 20,
-    class4: {
-        a: 20,
-        b: 18,
-        c: 13
-    },
-    class5: 30,
-    class6: {
-        a: 20,
-        b: 30,
-        c: 25
-    },
-    class7: 35,
-    class8: 26,
-    class9: {
-        a: 22,
-        b: 24,
-        c: 23
-    },
-    class10: {
-        a: 22,
-        b: 19,
-        c: 21
-    },
-    class11: {
-        a: 26,
-        b: 24,
-        c: 25
-    },
-    class12: {
-        a: 22,
-        b: 21,
-        c: 20
-    }
-}
-for (let i in school) {
-    if (typeof school[i] === "object") {
-        for (let j in school[i]) {
-            console.log(`There are ${school[i][j]} students in the ${i} - ${j}`);
-        }
-    } else {
-        console.log(`There are ${school[i]} students in the ${i}`);
-    }
-}
+"use strict";
 
-const fruits = ["Apple", "Banana", ["Orange", "Mango", "Mandarin"],
-    ["Kiwi", "Guava", "Grapes"], "Lime", ["Cherries", "Avocados"],
-    "Apricot", ["Tomato", "Tangerine"], "Grapefruit", ["Raspberry", "Bingberry"]];
-
-for (let i of fruits) {
-    if (typeof i === 'object') {
-        for (let j in i) {
-            console.log(i[j]);
-        }
-    } else {
-        console.log(i);
-    }
-}
-
-const obj = {
-    autoReturn() {
-        return 1 + 5 + 15;
+const myTaxiService = {
+    serviceName: "Classic",
+    carFirms: ["Mercedes", "BMW", "Lexus", "Mitsubishi"],
+    openHours: "6.00 - 24.00",
+    drivers: ["Karen", "Aram", "Robert", "Vachagan", "Sanasar"],
+    speed: ["Quicly", "Medium", "Slowly"],
+    corrections() {
+        myTaxiService.serviceName = myTaxiService.serviceName.toUpperCase();
+        myTaxiService.speed.forEach((element, index, arr) => {
+            arr[index] = element.toLowerCase();
+        });
     },
-    sumOfArray() {
-        const arr = [1, 4, 8, 8, 9, 4, 6, 6];
-        let sum = 0;
-        for (let i of arr) {
-            sum += i;
+    opening() {
+        myTaxiService.openHours = myTaxiService.openHours.split(" - ");
+        let thisDate = new Date();
+        let thisHour = thisDate.getHours();
+        if (thisHour <= myTaxiService.openHours[1] && thisHour >= myTaxiService.openHours[0]) {
+            myTaxiService.corrections();
+            myTaxiService.selectYourCar("      Mitsubishi   ");
+        } else {
+            console.log("Մենք փակ ենք:");
         }
-        return sum;
     },
-    maxMinArr() {
-        const arr = [1, -5, 8, 15, 6, 8, 4, 85];
-        let max = arr[0];
-        let min = arr[0];
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i]
-            }
-            if (arr[i] < min) {
-                min = arr[i];
+    selectYourCar(carFirm) {
+        carFirm = carFirm.trim();
+        for (let i = 0; i < myTaxiService.carFirms.length; i++) {
+            if (carFirm == myTaxiService.carFirms[i]) {
+                carFirm = myTaxiService.carFirms.slice(i, i + 1);
+                myTaxiService.carFirms = myTaxiService.carFirms.join(", ");
+                console.log(`${myTaxiService.carFirms} այս մեքենաներից դուք ընտրեցիք ${carFirm}-ը
+                Մեքենայի վարորդն է՝ ${myTaxiService.selectDriver()}-ը`);
             }
         }
-        console.log(min, max);
+    },
+    selectDriver(driver) {
+        driver = myTaxiService.drivers[Math.floor(Math.random() * 5)];
+        return driver;
     }
-
 }
-const { autoReturn, sumOfArray, maxMinArr } = obj;
-console.log(autoReturn(), sumOfArray(),);
-maxMinArr();
-
-const arr = [
-    function fibonachi(toNum) {
-        let x = [0, 1];
-        for (let i = 2; i < toNum; i++) {
-            x[i] = x[i - 1] + x[i - 2];
-        }
-        return x;
-    },
-    function factorial(toNum) {
-        let sum = 1;
-        for (let i = 1; i <= toNum; i++) {
-            sum *= i;
-        }
-        return sum;
-    },
-]
-
-const [fibonachi, factorial] = arr;
-console.log(fibonachi(20), factorial(10));
+myTaxiService.opening();
+console.log(myTaxiService);
