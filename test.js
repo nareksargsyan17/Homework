5f062ff12c2773ac084262d71562fc9b85686165
nareksargsyan17 <narek.sargsyan.2000.g@gmai.com>
2022-07-21 13:23:02 +0400

    new homework

--- test.js
+++ test.js
@@ -1,59 +1,44 @@
-function counter(count){
-    count = 1;
-    function counterPlus(){
-        count++;
-        function counterMinus(){
-            count--;
-            return count;
-        }
-        return counterMinus;
-    }
-    return counterPlus;
+const header = document.querySelector("#header");
+console.log(header);
+header.style.backgroundColor = "grey";
+const menu = document.querySelectorAll(".menu");
+console.log(menu);
+menu.forEach(element => {
+    element.style.border = "2px solid black";
+});
+const dives = document.querySelector("#dives");
+dives.style.borderRadius = "5%";
+const childrens = document.querySelectorAll(".childrens");
+childrens.forEach(element => {
+    element.style.borderRadius = "10% 100%";
+    element.style.textDecoration = "underline";
+    element.style.fontSize = "20px";
+    element.style.textAlign = "right";
+    element.style.color = "red";
+});
+const menuNames = document.querySelectorAll("[name = 'menuNames']");
+menuNames.forEach(element => {
+    element.style.color = "white";
+});
+let lorem = document.querySelector("p[name = 'lorem']");
+console.log(lorem.textContent);
+let loremText = lorem.textContent.trim();
+// console.log(loremText);
+// for(let i = 0; i < loremText.length; i++){
+//     if(loremText[i] == " "){
+//         loremText[i+1] = loremText.toUpperCase();
+//         console.log(loremText[i+1]);
+//     }
+// }
+// console.log(loremText);
+lorem.style.color = "red";
+const numbers = document.querySelector("#numbers");
+numbers.style.color = "blue";
+numbers.style.backgroundColor = "aliceblue";
+numbers.style.display = "flex";
+numbers.style.flexDirection = "column-reverse"
+const num = document.querySelectorAll(".num");
+for(let i = 0; i<num.length; i++){
+    num[i].style.color = "orange";
 }
-
-const count = counter();
-console.log(count()());
-
-function oneDB(name, lastName, age){
-    function forName(){
-        name = "Petros";
-        return name;
-    }
-    function forLastName(){
-        lastName = "Kirakosyan";
-        return lastName;
-    }
-    function forAge(){
-        age = 27;
-        return age;
-    }
-    return {
-        name : forName,
-        lastName : forLastName,
-        age : forAge
-    };
-}
-const res = oneDB();
-console.log(res.name());
-console.log(res.lastName());
-console.log(res.age());
-
-function tester(){
-    let count = 0;
-    function counter(){
-        count++;
-        function counter1(){
-            count++;
-            return count;
-        }
-        return counter1;
-    }
-    return counter;
-}
-const testerCount = tester()();
-console.log(testerCount());
-console.log(testerCount());
-console.log(testerCount());
-console.log(testerCount());
-
 
