0f4cb94ed7491dc0be8955fdfbfac68f9e9e9280
nareksargsyan17 <narek.sargsyan.2000.g@gmai.com>
2022-08-10 14:12:27 +0400

    holidays homework

--- test.js
+++ test.js
@@ -1,46 +1,36 @@
-const header = document.querySelector("#header");
-console.log(header);
-header.style.backgroundColor = "grey";
-const menu = document.querySelectorAll(".menu");
-console.log(menu);
-menu.forEach(element => {
-    element.style.border = "2px solid black";
-});
-const dives = document.querySelector("#dives");
-dives.style.borderRadius = "5%";
-const childrens = document.querySelectorAll(".childrens");
-childrens.forEach(element => {
-    element.style.borderRadius = "10% 100%";
-    element.style.textDecoration = "underline";
-    element.style.fontSize = "20px";
-    element.style.textAlign = "right";
-    element.style.color = "red";
-});
-const menuNames = document.querySelectorAll("[name = 'menuNames']");
-menuNames.forEach(element => {
-    element.style.color = "white";
-});
-let lorem = document.querySelector("p[name = 'lorem']");
-console.log(lorem.textContent);
-let loremText = lorem.textContent.trim();
-loremText = loremText.split("");
-
-console.log(loremText);
-for(let i = 0; i < loremText.length; i++){
-    if(loremText[i] == " "){
-        loremText[i+1] = loremText[i+1].toUpperCase();
-        console.log(loremText[i+1]);
+const div = document.querySelector("#div");
+div.style.marginTop = "200px";
+div.style.marginLeft = "700px";
+console.dir(window);
+addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 's': for (let i = 0; i < 50; i++) {
+            if (parseInt(div.style.marginTop) + div.clientHeight > window.innerHeight) {
+                div.style.marginTop = `${window.innerHeight - div.clientHeight}px`;
+            } else {
+                div.style.marginTop = `${parseInt(div.style.marginTop) + 1}px`;
+            }
+        }; break;
+        case 'w': for (let i = 0; i < 50; i++) {
+            if (parseInt(div.style.marginTop) < 0) {
+                div.style.marginTop = `0px`;
+            } else {
+                div.style.marginTop = `${parseInt(div.style.marginTop) - 1}px`;
+            }
+        }; break;
+        case 'a': for (let i = 0; i < 50; i++) {
+            if (parseInt(div.style.marginLeft) < 0 || parseInt(div.style.marginLeft) > window.innerWidth) {
+                div.style.marginLeft = '0px';
+            } else {
+                div.style.marginLeft = `${parseInt(div.style.marginLeft) - 1}px`;
+            }
+        } break;
+        case 'd': for (let i = 0; i < 50; i++) {
+            if (parseInt(div.style.marginLeft) + div.clientWidth > window.innerWidth) {
+                div.style.marginLeft = `${window.innerWidth - div.clientWidth}px`;
+            } else {
+                div.style.marginLeft = `${parseInt(div.style.marginLeft) + 1}px`;
+            }
+        } break;
     }
-}
-console.log(loremText.join(""));
-lorem.style.color = "red";
-const numbers = document.querySelector("#numbers");
-numbers.style.color = "blue";
-numbers.style.backgroundColor = "aliceblue";
-numbers.style.display = "flex";
-numbers.style.flexDirection = "column-reverse"
-const num = document.querySelectorAll(".num");
-for(let i = 0; i<num.length; i++){
-    num[i].style.color = "orange";
-}
-
+})
