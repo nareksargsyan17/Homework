95558c39e2a1effb44fc4f13dcb02fb5c08a2ccc
nareksargsyan17 <narek.sargsyan.2000.g@gmai.com>
2022-08-25 13:33:46 +0400

    new events(touches and drags)

--- test.js
+++ test.js
@@ -1,164 +1,43 @@
-//1
-function factorial(num, sum = 1) {
-    if (num > 0) {
-        sum = num * factorial(num - 1);
-    }
-    return sum;
-}
-console.log(factorial(7));
-
-function fibonachi(num, arr = [1, 2]) {
-    if (arr[arr.length - 1] + arr[arr.length - 2] <= num) {
-        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
-        fibonachi(num, arr);
-    }
-    return arr;
-}
-console.log(fibonachi(500));
-
-//2
-
-const nameArr = ["Karen", "Armen", "Sevak", "Hrayr", "Karine", "Armine"];
-function printNames(arr) {
-    if (arr.length !== 0) {
-        console.log(arr.pop());
-        printNames(arr);
-    }
-}
-printNames(nameArr);
-
-const cars = [
-    {
-        name: "Toyota Camry",
-        price: 5000,
-        color: "grey",
-        speed: 140
-    },
-    {
-        name: "Mercedes C180",
-        price: 4000,
-        color: "grey",
-        speed: 120
-    },
-    {
-        name: "BMW E60",
-        price: 6000,
-        color: "red",
-        speed: 180
-    },
-    {
-        name: "Lexus RX470",
-        price: 8000,
-        color: "green",
-        speed: 170
-    },
-    {
-        name: "Chevrolet Dodge",
-        price: 5000,
-        color: "black",
-        speed: 130
-    }
-]
-console.table(cars);
-
-function selectYourCar(arr, i = 0) {
-    if (arr[i].price === 5000 && arr[i].color === "grey") {
-        console.log(arr[i]);
-    } else {
-        i++;
-        selectYourCar(arr, i)
-    }
-}
-selectYourCar(cars)
-
-const arr = [5, 5, 7, 8, 9, 8, 5, 2, 0, 4, 1, 40, 1, 25, 48, 89, 52, 2, 55, 56, 66, 4];
-function findMax(arr, i = 1, max = arr[0]) {
-    if (arr[i] > max) {
-        max = arr[i];
-    }
-    if (i < arr.length) {
-        i++;
-        findMax(arr, i, max);
-    } else {
-        console.log(max);
-    }
-}
-findMax(arr);
-
-const namesArr = ["Karen", "Armen", "Sevak", "Hrayr", "Karine", "Armine", "Samvel", "Axasi", "Arpine", "Sos"];
-const body0 = document.body;
-function namesSelect(arr, i = 0) {
-    if (i % 2 === 1) {
-        body0.insertAdjacentHTML("afterbegin", `
-        <h1>${arr[i]}</h1>
-        `)
-    }
-    i++;
-    if (i < arr.length) {
-        namesSelect(arr, i);
-    }
-}
-namesSelect(nameArr);
-
-const numbers = [5, 5, 7, 8, 9, 8, 5, 2, 0, 4, 1, 40, 1, 25, 48, 89, 52, 2, 55, 56, 66, 4];
-function numbersSum(arr, i = 0, sum = 0) {
-    if (arr[i] % 3 === 0) {
-        sum += arr[i];
-    }
-    if (i < arr.length) {
-        i++;
-        numbersSum(arr, i, sum);
-    } else {
-        console.log(sum);;
-    }
-}
-numbersSum(numbers);
-
-//3
-
-const body = document.body;
-const div1 = document.createElement('div');
-body.insertAdjacentElement("afterbegin", div1);
-div1.insertAdjacentText("afterbegin", "Welcome!");
-div1.style.cssText = `
-    width: 160px;
-    height: 80px;
-    color: black;
-    font-size: 35px;
-    margin-left:40%;
-`;
-body.insertAdjacentHTML("beforeend", `
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-    Voluptas, sunt, ut fugit qui illo ipsam esse impedit explicabo ea molestias quo cumque 
-    magnam commodi harum architecto voluptatibus deleniti corporis. Doloribus.</p>
-`);
-div1.insertAdjacentHTML("afterend", `
-    <img src = "https://2.bp.blogspot.com/_CjOU7NyqN18/TMFzXu3oWlI/AAAAAAAACB4/0ewXBActfjY/s640/783591926_1280x1024.jpg" style="margin-left:25%"></img>
-`)
-
-// 4
-
-const arrNames = ["Karen", "Armen", "Sevak", "Hrayr", "Karine", "Armine", "Samvel", "Axasi", "Arpine",
-    "Sos", "Anna", "Ani", "Valodya", "Misha", "Karapet"];
-const names = document.querySelector("#div");
-const input = names.querySelector('input');
-const div = names.querySelector('div')
-arrNames.forEach((element) => {
-    div.insertAdjacentHTML("beforeend", `
-    <p>${element}</p>
-`)
-})
-input.addEventListener("keyup", () => {
-    let count = 0;
-    for (let i = 0; i < arrNames.length; i++) {
-        if (arrNames[i].toLowerCase().search(input.value.toLowerCase()) != -1) {
-            div.children[i].textContent = arrNames[i];
-            count++;
+const div1 = document.querySelector("#div1");
+const div2 = document.querySelector("#div2");
+let y2, x1, y1, x2;
+div1.addEventListener("touchmove", ev);
+div2.addEventListener("touchmove", ev);
+function ev(e) {
+    e.target.style.cssText = `
+    transform : translateX(${e.touches[0].pageX}px) translateY(${e.touches[0].pageY}px);
+    `
+    if (e.target.id == "div1") {
+        x1 = e.touches[0].pageX;
+        y1 = e.touches[0].pageY;
+        if (x1 + 100 >= x2 && x1 <= x2 + 100 && y1 + 100 >= y2 && y1 <= y2 + 100) {
+            div1.style.background = "yellow"
+            div2.style.background = "green"
         } else {
-            div.children[i].textContent = "";
+            div2.style.background = "yellow"
+        }
+    } else {
+        x2 = e.touches[0].pageX;
+        y2 = e.touches[0].pageY;
+        if (x2 + 100 >= x1 && x2 <= x1 + 100 && y2 + 100 >= y1 && y2 <= y1 + 100) {
+            div1.style.background = "yellow"
+            div2.style.background = "green"
+        } else {
+            div1.style.background = "green"
         }
     }
-    if (count === 0) {
-        div.children[0].textContent = "Not finding";
-    }
+}
+
+// 2 harc
+const someDiv = document.querySelector("#someDiv");
+someDiv.addEventListener("dragstart", () => {
+    console.log("onDrag");
+})
+someDiv.addEventListener("drag", () => {
+    console.log("Draging..");
+})
+someDiv.addEventListener("dragend", (e) => {
+    someDiv.style.cssText = `
+    transform: translateX(${e.pageX -100}px) translateY(${e.pageY}px)`
+    console.log("dragEnd");
 })
