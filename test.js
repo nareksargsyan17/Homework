3cf8ed099a90bf01751e800e0c776c78b2d5025b
nareksargsyan17 <narek.sargsyan.2000.g@gmai.com>
2022-09-01 14:49:55 +0400

    timer

--- test.js
+++ test.js
@@ -1,43 +1,38 @@
-const div1 = document.querySelector("#div1");
-const div2 = document.querySelector("#div2");
-let y2, x1, y1, x2;
-div1.addEventListener("touchmove", ev);
-div2.addEventListener("touchmove", ev);
-function ev(e) {
-    e.target.style.cssText = `
-    transform : translateX(${e.touches[0].pageX}px) translateY(${e.touches[0].pageY}px);
-    `
-    if (e.target.id == "div1") {
-        x1 = e.touches[0].pageX;
-        y1 = e.touches[0].pageY;
-        if (x1 + 100 >= x2 && x1 <= x2 + 100 && y1 + 100 >= y2 && y1 <= y2 + 100) {
-            div1.style.background = "yellow"
-            div2.style.background = "green"
-        } else {
-            div2.style.background = "yellow"
-        }
-    } else {
-        x2 = e.touches[0].pageX;
-        y2 = e.touches[0].pageY;
-        if (x2 + 100 >= x1 && x2 <= x1 + 100 && y2 + 100 >= y1 && y2 <= y1 + 100) {
-            div1.style.background = "yellow"
-            div2.style.background = "green"
-        } else {
-            div1.style.background = "green"
-        }
+function someDate(someDay, someMonth) {
+    const text = document.querySelector("#h2")
+    const date = new Date();
+    const newDate = new Date();
+    newDate.setMonth(someMonth);
+    newDate.setDate(someDay);
+    if (newDate > date) {
+        newDate.setFullYear(2021);
     }
+    let day = (date - newDate) / 1000 / 3600 / 24;
+    let hour = 0;
+    let minutes = 0;
+    let seconds = 1;
+    const timer = setInterval(() => {
+        seconds--;
+        if (day == 0 && hour == 0 && minutes == 0 && seconds == 0) {
+            console.log("done");
+            clearInterval(timer)
+        } else {
+            if (hour == 0 && minutes == 0 && seconds == 0) {
+                hour = 24;
+                day--;
+            }
+            if (minutes == 0 && seconds == 0) {
+                minutes = 60;
+                hour--;
+            }
+            if (seconds == 0) {
+                seconds = 60;
+                minutes--;
+            }
+        }
+        text.textContent = `
+        ${day} : ${hour} : ${minutes} : ${seconds}
+        `
+    }, 1000)
 }
-
-// 2 harc
-const someDiv = document.querySelector("#someDiv");
-someDiv.addEventListener("dragstart", () => {
-    console.log("onDrag");
-})
-someDiv.addEventListener("drag", () => {
-    console.log("Draging..");
-})
-someDiv.addEventListener("dragend", (e) => {
-    someDiv.style.cssText = `
-    transform: translateX(${e.pageX -100}px) translateY(${e.pageY}px)`
-    console.log("dragEnd");
-})
+someDate(31, 7)
